3720feed6c4185db07fd259bf6a8d9de3ad0ced3
IdirAm <idiramrouche2@gmail.com>
2020-11-13 13:43:31 +0100

    first commit

---
+++ src/test/java/com/projet/mvc/ProjetTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
